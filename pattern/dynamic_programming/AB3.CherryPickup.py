4103c7c871a2ac19e0ed4e5d285ef30b420659e4
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-07-05 16:07:12 +0530

    Update AB3.CherryPickup.py

--- pattern/dynamic_programming/AB3.CherryPickup.py
+++ pattern/dynamic_programming/AB3.CherryPickup.py
@@ -34,4 +34,48 @@ class Solution:
             return maxi
         
         return function(0, 0, m-1)
+
+
+#bottom up
+class Solution:
+    def solve(self, n, m, grid):
+        
+        # 3d array
+        dp = [ [ [ 0 for _ in range(m) ] for _ in range(m) ] for _ in range(n) ]
+        
+        #base case - independent case
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 == j2:
+                    dp[n-1] [j1][j2] = grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]
+        
+        #subproblem
+        for i in range(n-2, -1, -1):
+            for j1 in range(m):
+                for j2 in range(m):
                     
+                    # now p1 in (i, j1) and p2 in (j,j2)
+                    # current cost
+                    if j1 == j2:
+                        ans = grid[i][j1]
+                    else:
+                        ans = grid[i][j1] + grid[i][j2]
+                        
+                    # find best previous, we would use.
+                    maxi = float('-inf')
+                    #all positions i and j could have come from.
+                    for di in range(-1,2):
+                        for dj in range(-1,2):
+                            new_j1 = j1+di
+                            new_j2 = j2+dj
+                            
+                            if 0<=new_j1<m and 0<=new_j2<m:
+                                maxi = max(maxi,dp[i+1][new_j1][new_j2])
+                                
+                    dp[i][j1][j2] = maxi + ans
+                    
+
+        return dp[0][0][m-1] 
+

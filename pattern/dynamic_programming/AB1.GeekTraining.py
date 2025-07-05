0b8734309afc4268e907eb95b671e121aee5d507
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2025-07-05 15:06:44 +0530

    Update AB1.GeekTraining.py

--- pattern/dynamic_programming/AB1.GeekTraining.py
+++ pattern/dynamic_programming/AB1.GeekTraining.py
@@ -24,3 +24,25 @@ class Solution:
             
         return function(0,-1)
             
+class Solution:
+    def maximumPoints(self, arr):
+        
+        N = len(arr)
+        
+        dp = [[0 for _ in range(3)] for i in range(N)]
+        
+        #day 1 task
+        for i in range(3):
+            dp[0][i] = arr[0][i]
+            
+        
+        for day in range(1, N):#each day
+            for task in range(3):#each task
+                max_prev = 0
+                for prev in range(3):#consider each prev
+                    if task == prev:continue
+                    max_prev = max(max_prev, dp[day-1][prev])
+                dp[day][task] = arr[day][task] + max_prev
+        
+        return max(dp[N-1][0],dp[N-1][1],dp[N-1][2])
+        
